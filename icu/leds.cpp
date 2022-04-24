9e85113763932f5b416fa86667a8556926784d5a
vsawce <vsaw30@gmail.com>
2022-04-24 04:14:49 -0700

    Began migration away from SW SPI LedControl library to HW SPI MD_MAX72XX

--- icu/leds.cpp
+++ icu/leds.cpp
@@ -1,24 +1,26 @@
 #include "leds.h"
 
-const uint8_t PICO_MOSI_MAX7219 = 19;
-const uint8_t PICO_CLK_MAX7219 = 18;
-const uint8_t PICO_CS_MAX7219 = 13;
-
-LedControl lc=LedControl(PICO_MOSI_MAX7219, PICO_CLK_MAX7219, PICO_CS_MAX7219, 1);
+MD_MAX72XX *leds;
 
 unsigned long delaytime=65;
 
+void leds__init(MD_MAX72XX *leds_ptr) {
+  leds = leds_ptr;
+  leds->begin();
+}
+
+/*
 void leds__wake() {
   lc.shutdown(0,false);
-  /* Set the brightness to a medium values */
+  // Set the brightness to a medium values
   lc.setIntensity(0,8);
-  /* and clear the display */
+  // and clear the display
   lc.clearDisplay(0);
 }
 
 void leds__start(){
  
- /*
+ 
   lc.setLed(0,0,0,true); // First RGB
   lc.setLed(0,0,3,true); // Second RGB
   lc.setLed(0,1,1,true); // Third RGB
@@ -28,7 +30,7 @@ void leds__start(){
 
   lc.setLed(0,2,0,true); // Right Top LED
   lc.setLed(0,2,5,true); // Left Top LED
-*/
+
 
   for(int row = 0; row < 5; row++){
       for(int col = 0; col < 10; col++){
@@ -47,3 +49,4 @@ void leds__start(){
   
 
 }
+*/
--- icu/leds.h
+++ icu/leds.h
@@ -1,10 +1,11 @@
 #include <Arduino.h>
-#include <LedControl.h>
+#include <MD_MAX72xx.h>
+//#include <LedControl.h>
 
 #ifndef LEDS_H_
 #define LEDS_H_
 
-
+void leds__init(MD_MAX72XX *leds_ptr);
 void leds__wake();
 void leds__start();
 
