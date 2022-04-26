d72efe696b02a71be991c5635fd6b3028b5718da
vsawce <vsaw30@gmail.com>
2022-04-26 02:08:28 -0700

    Added pin mapping definitions for ICU Rev A Added function leds__rpm_update(uint16_t rpm) which turns leds based on a fat switch function, and turns off unused leds

--- icu/leds.cpp
+++ icu/leds.cpp
@@ -9,6 +9,60 @@ void leds__init(MD_MAX72XX *leds_ptr) {
   leds->begin();
 }
 
+void leds__wake() {
+  
+}
+
+void leds__rpm_update(uint16_t rpm) {
+  uint8_t leds_to_turn_off;
+  uint8_t leds_to_turn_on = 0;
+  switch(rpm) {
+    case 500 ... 2749:
+      leds_to_turn_on = 1;
+      break;
+    case 2750 ... 4499:
+      leds_to_turn_on = 2;
+      break;
+    case 4500 ... 5999:
+      leds_to_turn_on = 3;
+      break;
+    case 6000 ... 6999:
+      leds_to_turn_on = 4;
+      break;
+    case 7000 ... 7999:
+      leds_to_turn_on = 5;
+      break;
+    case 8000 ... 8999:
+      leds_to_turn_on = 6;
+      break;
+    case 9000 ... 9999:
+      leds_to_turn_on = 7;
+      break;
+    case 10000 ... 10999:
+      leds_to_turn_on = 8;
+      break;
+    case 11000 ... 11999:
+      leds_to_turn_on = 9;
+      break;
+    case 12000 ... 16000:
+      leds_to_turn_on = 10;
+      break;
+    default:
+      leds_to_turn_on = 0;
+      break;
+  }
+  //Inverse of # leds ON
+  leds_to_turn_off = NUM_LED_SOLID-leds_to_turn_on;
+  //Turn on LEDS
+  for (uint8_t led_idx = 0; led_idx < leds_to_turn_on; led_idx++) {
+    leds->setPoint(PIN_LED_SOLID[led_idx][0],PIN_LED_SOLID[led_idx][1],true);
+  }
+  //Turn off unused LEDS
+  for (uint8_t led_idx = 0; led_idx < leds_to_turn_off; led_idx++) {
+    leds->setPoint(PIN_LED_SOLID[NUM_LED_SOLID-led_idx][0],PIN_LED_SOLID[NUM_LED_SOLID-led_idx][1],false);
+  }
+}
+
 /*
 void leds__wake() {
   lc.shutdown(0,false);
--- icu/leds.h
+++ icu/leds.h
@@ -5,9 +5,28 @@
 #ifndef LEDS_H_
 #define LEDS_H_
 
+/*---------------------------------------------------------------------------/
+/ LED MAP TO MAX7219 ROW/COLUMN
+/---------------------------------------------------------------------------*/
+// Solid state LED map
+// 10 array rows per Solid State LED, 2 array columns for MAX7219 row & column # respectively
+const uint8_t NUM_LED_SOLID = 10;
+const uint8_t NUM_LED_RGB = 6;
+const uint8_t PIN_LED_SOLID[NUM_LED_SOLID][2] = {{0,0}, {0,1}, {0,2}, {0,3}, {0,4},
+                                                {0,5}, {1,1}, {1,2}, {1,3}, {1,4}};
+
+// RGB LED map (Todo)
+// 6 array rows per RGB LED number (red only), 2 array columns per MAX7219 {row, column}
+//const uint8_t PIN_LED_RGB_R[NUM_LED_RGB][2];
+// 6 array rows per RGB LED number (green only), 2 array columns per MAX7219 {row, column}
+//const uint8_t PIN_LED_RGB_G[NUM_LED_RGB][2];
+// 6 array rows per RGB LED number (blue only), 2 array columns per MAX7219 {row, column}
+//const uint8_t PIN_LED_RGB_B[NUM_LED_RGB][2];
+
 void leds__init(MD_MAX72XX *leds_ptr);
 void leds__wake();
 void leds__start();
+void leds__rpm_update(uint16_t rpm);
 
 //create LEDs Update function that takes CAN data and changes LEDs
 
