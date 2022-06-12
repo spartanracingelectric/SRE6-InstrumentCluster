a7bdcf37cb81422c5a0fdbf5492e10fdda473e07
KueTone <66387900+KueTone@users.noreply.github.com>
2022-06-12 14:57:05 -0700

    Updated driver screen and added menu screen

--- icu/lcd.h
+++ icu/lcd.h
@@ -6,10 +6,10 @@
 #include <SPI.h>
 #include "config.h"
 
-void lcd__init(U8G2_ST7565_NHD_C12864_F_4W_SW_SPI *lcd_ptr);
+void lcd__init(U8G2_ST7565_NHD_C12864_F_4W_SW_SPI *lcd_ptr); // changed from SW -> HW
 void lcd__clear_screen();
-void lcd__print8(uint8_t x, uint8_t y, char *str);                 //Print str with Helvetica Bold Size 10
-void lcd__print14(uint8_t x, uint8_t y, char *str);                 //Print str with Helvetica Bold Size 14
+void lcd__print8(uint8_t x, uint8_t y, const char *str);                 //Print str with Helvetica Bold Size 10
+void lcd__print14(uint8_t x, uint8_t y, const char *str);                 //Print str with Helvetica Bold Size 14
 void lcd__print18(uint8_t x, uint8_t y, char *str);                 //Print str with Helvetica Bold Size 18
 void lcd__print24(uint8_t x, uint8_t y, char *str);                 //Print str with Helvetica Bold Size 24
 void lcd__print_default_screen_template();
@@ -17,17 +17,21 @@ void lcd__print_template();
 // C car
 void lcd__print_rpm(uint16_t rpm);
 void lcd__print_gear(uint8_t gear);
+void lcd__print_oiltemp(uint8_t oiltemp); // Oil coolant? temperature
 // E car
 void lcd__print_hv(uint16_t hv); // accumulator voltage
 void lcd__print_soc(uint8_t soc); // State of charge 0-100%
+void lcd__print_wattemp(uint8_t watertemp); // Water Coolant Temp
 // E & C car
 void lcd__print_lv(uint8_t lv); // low voltage battery soc
 void lcd__print_etemp(uint8_t etemp); // Accumulator/Engine temperature
-void lcd__print_oiltemp(uint8_t oiltemp); // Oil coolant? temperature
-void lcd__print_drs(uint8_t drs); // Drag Reduction System: Open or Closed - CAN: 0 or 1
+void lcd__rpm_screen(void); // Change RPM Threshold
 //void lcd_print__tiretemp(uint8_t tiretemp); // Tire temperatures multiple variables?
 
-
+// Menu stuff
+void lcd__print_menu(void);
+void lcd__highlight_menu(uint8_t row, const char* screen);
+void lcd__drs_screen(uint8_t drs);
 
 void lcd__update_screen(uint16_t rpm, uint8_t gear, uint8_t lv, uint8_t etemp, uint8_t oiltemp, uint8_t drs, uint32_t curr_millis_lcd);
 void lcd__update_screenE(uint16_t hv, uint8_t soc, uint8_t lv, uint8_t etemp, uint8_t oiltemp, uint8_t drs, uint32_t curr_millis_lcd);
