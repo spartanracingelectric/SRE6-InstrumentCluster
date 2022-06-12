c0640f71d8da51beb255aa653f1c46b1476a302a
KueTone <66387900+KueTone@users.noreply.github.com>
2022-06-12 14:53:11 -0700

    Added more CAN functions

--- icu/can.h
+++ icu/can.h
@@ -9,8 +9,26 @@
 void can__start();
 void can__send_test();
 void can__receive();
-uint16_t can__get_rpm();
-uint8_t can__get_gear();
+
+// C car
+uint16_t can__get_rpm(const CANMessage & inMessage);
+uint8_t can__get_gear(const CANMessage & inMessage);
+uint8_t can__get_oiltemp(const CANMessage & inMessage);
+uint8_t can__get_engtemp(const CANMessage & inMessage); // C car engine
+
+// E car
+uint16_t can__get_hv(const CANMessage & inMessage);
+uint8_t can__get_soc(const CANMessage & inMessage);
+uint8_t can__get_wattemp(const CANMessage & inMessage);
+uint8_t can__get_acctemp(const CANMessage & inMessage); // E car accumulator
+
+// E & C car
+uint8_t can__get_lv(const CANMessage & inMessage);
+uint8_t can__get_drs(const CANMessage & inMessage);
+
+
+
+
 void can__stop();
 
 
