0315e692e7771e54f842e0f2f9de5ea908b55f9e
KueTone <66387900+KueTone@users.noreply.github.com>
2022-06-12 19:34:19 -0700

    Fixed incorrect function declarations

--- icu/can.h
+++ icu/can.h
@@ -11,20 +11,20 @@ void can__send_test();
 void can__receive();
 
 // C car
-uint16_t can__get_rpm(const CANMessage & inMessage);
-uint8_t can__get_gear(const CANMessage & inMessage);
-uint8_t can__get_oiltemp(const CANMessage & inMessage);
-uint8_t can__get_engtemp(const CANMessage & inMessage); // C car engine
+uint16_t can__get_rpm();
+uint8_t can__get_gear();
+uint8_t can__get_oiltemp();
+uint8_t can__get_engtemp(); // C car engine
 
 // E car
-uint16_t can__get_hv(const CANMessage & inMessage);
-uint8_t can__get_soc(const CANMessage & inMessage);
-uint8_t can__get_wattemp(const CANMessage & inMessage);
-uint8_t can__get_acctemp(const CANMessage & inMessage); // E car accumulator
+uint16_t can__get_hv();
+uint8_t can__get_soc();
+uint8_t can__get_wattemp();
+uint8_t can__get_acctemp(); // E car accumulator
 
 // E & C car
-uint8_t can__get_lv(const CANMessage & inMessage);
-uint8_t can__get_drs(const CANMessage & inMessage);
+uint8_t can__get_lv();
+uint8_t can__get_drs();
 
 
 
