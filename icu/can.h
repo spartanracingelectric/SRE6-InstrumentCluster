af070fc94bba8073b55713fc3e0dac2abaf21a56
KueTone <66387900+KueTone@users.noreply.github.com>
2022-06-16 00:50:55 -0400

    Organized and deleted unnecessary functions

--- icu/can.h
+++ icu/can.h
@@ -11,25 +11,33 @@ void can__send_test();
 void can__receive();
 
 // C car
+#if (POWERTRAIN_TYPE == 'C')
+static void can__rpm_receive (const CANMessage & inMessage);
+static void can__gear_receive (const CANMessage & inMessage);
+static void can__oilpress_receive (const CANMessage & inMessage);
+static void can__lv_receive (const CANMessage & inMessage);
+static void can__drs_receive (const CANMessage & inMessage);
+
 uint16_t can__get_rpm();
 uint8_t can__get_gear();
-uint8_t can__get_oiltemp();
-uint8_t can__get_engtemp(); // C car engine
-
-// E car
-uint16_t can__get_hv();
-uint8_t can__get_soc();
-uint8_t can__get_wattemp();
-uint8_t can__get_acctemp(); // E car accumulator
-
-// E & C car
-uint8_t can__get_lv();
+float can__get_oilpress();
+float can__get_lv();
 uint8_t can__get_drs();
 
+// E car
+#elif (POWERTRAIN_TYPE == 'E')
+static void can__hv_receive (const CANMessage & inMessage);
+static void can__soc_receive (const CANMessage & inMessage);
+static void can__lv_receive (const CANMessage & inMessage);
+static void can__acctemp_receive (const CANMessage & inMessage);
 
+float can__get_hv();
+float can__get_soc();
+float can__get_lv();
+float can__get_acctemp(); // E car accumulator
 
+#endif
 
 void can__stop();
-
 
 #endif /* CAN_H_ */
