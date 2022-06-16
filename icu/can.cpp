30b378cf9552e76ef6824346178202160bf8843a
KueTone <66387900+KueTone@users.noreply.github.com>
2022-06-16 00:50:20 -0400

    Separated C and E functions and variables

--- icu/can.cpp
+++ icu/can.cpp
@@ -11,18 +11,21 @@ ACAN2515 can (PICO_CAN_SPI_CS, SPI1, PICO_CAN_INT);
 static const uint32_t QUARTZ_FREQUENCY = 16UL * 1000UL * 1000UL; // 16 MHz
 ACAN2515Settings settings (QUARTZ_FREQUENCY, 500UL * 1000UL) ; // CAN bit rate 500s kb/s
 
+#if (POWERTRAIN_TYPE == 'C')
 uint16_t curr_rpm = 0;
 uint8_t curr_gear = 0;
-uint8_t curr_oiltemp = 0;
-uint8_t curr_engtemp = 0;
-
-uint16_t curr_hv = 0;
-uint8_t curr_soc = 0;
-uint8_t curr_wattemp = 0;
-uint8_t curr_acctemp = 0;
-
-uint8_t curr_lv = 0;
+float curr_oilpress = 0;
+float curr_lv = 0;
 uint8_t curr_drs = 0;
+
+/*
+static void can__dummy_receive (const CANMessage & inMessage)
+{
+  uint8_t durr;
+  //curr_gear = inMessage.data[1];
+  //Serial.println ("Received Gear " + curr_gear) ;
+}
+*/
 
 static void can__rpm_receive (const CANMessage & inMessage)
 {
@@ -36,81 +39,22 @@ static void can__gear_receive (const CANMessage & inMessage)
   //Serial.println ("Received Gear " + curr_gear) ;
 }
 
-static void can__oiltemp_receive (const CANMessage & inMessage)
+static void can__oilpress_receive (const CANMessage & inMessage)
 {
-  curr_oiltemp = inMessage.data[1];
-}
-
-static void can__engine_receive (const CANMessage & inMessage)
-{
-  curr_engtemp = inMessage.data[1];
-  //Serial.println ("Received Gear " + curr_gear) ;
-}
-
-static void can__hv_receive (const CANMessage & inMessage)
-{
-  curr_hv = ((inMessage.data[4]) | (inMessage.data[5] << 8) | (inMessage.data[6] << 16) | (inMessage.data[7] << 24)) * .001f;
-}
-
-static void can__soc_receive (const CANMessage & inMessage)
-{
-  curr_soc = inMessage.data[1];
-}
-
-static void can__wattemp_receive (const CANMessage & inMessage)
-{
-  curr_wattemp = inMessage.data[1];
-}
-
-static void can__acctemp_receive (const CANMessage & inMessage)
-{
-  curr_acctemp = inMessage.data[1];
+//  curr_oilpress = inMessage.data[1];
 }
 
 static void can__lv_receive (const CANMessage & inMessage)
 {
-  curr_lv = ((inMessage.data[0]) | (inMessage.data[1] << 8)) * 0.001f;
+//  curr_lv = ((inMessage.data[0]) | (inMessage.data[1] << 8)) * 0.001f; // for e car
 }
 
 static void can__drs_receive (const CANMessage & inMessage)
 {
-  curr_drs = inMessage.data[1];
+//  curr_drs = inMessage.data[1]; // dummy line
 }
 
-//static void can__dummy_receive (const CANMessage & inMessage)
-//{
-//  uint8_t durr;
-//  //curr_gear = inMessage.data[1];
-//  //Serial.println ("Received Gear " + curr_gear) ;
-//}
-
-const ACAN2515Mask rxm0 = standard2515Mask (0x7FF, 0, 0) ;
-//const ACAN2515Mask rxm1 = standard2515Mask (0x7FF, 0, 0) ;
-
-// POWERTRAIN_TYPE == 'C'
-#if (POWERTRAIN_TYPE == 'C')
-const ACAN2515AcceptanceFilter filters [] =
-{
-  {standard2515Filter (CAN_RPM_ADDR, 0, 0), can__rpm_receive}, // RXF0
-  {standard2515Filter (CAN_GEAR_ADDR, 0, 0), can__gear_receive} // RXF1
-  //{standard2515Filter (0x7FE, 0, 0), can__dummy_receive}, // RXF2
-} ;
-// POWERTRAIN_TYPE == 'E'
-#elif (POWERTRAIN_TYPE == 'E')
-const ACAN2515AcceptanceFilter filters [] =
-{
-  {standard2515Filter (CAN_HV_ADDR, 0, 0), can__hv_receive},
-  {standard2515Filter (CAN_SOC_ADDR, 0, 0), can__soc_receive},
-//  {standard2515Filter (CAN_GEAR_ADDR, 0, 0), can__wattemp_receive}, // not defined
-//  {standard2515Filter (CAN_GEAR_ADDR, 0, 0), can__drs_receive}, // not defined
-//  {standard2515Filter (CAN_GEAR_ADDR, 0, 0), can__oiltemp_receive}, // not defined
-  {standard2515Filter (CAN_BAT_TEMP_ADDR, 0, 0), can__acctemp_receive},
-  {standard2515Filter (CAN_LV_ADDR, 0, 0), can__lv_receive}
-} ;
-#endif
-
-
-// C car
+// Accessors
 uint16_t can__get_rpm()
 {
   return curr_rpm;
@@ -121,42 +65,92 @@ uint8_t can__get_gear()
   return curr_gear;
 }
 
-uint8_t can__get_oiltemp()
+float can__get_oilpress()
 {
-  return curr_oiltemp;
-}
-uint8_t can__get_engtemp() // C car engine
-{
-  return curr_engtemp;
+  return curr_oilpress;
 }
 
-// E car
-uint16_t can__get_hv()
-{
-  return curr_hv;
-}
-uint8_t can__get_soc()
-{
-  return curr_soc;
-}
-uint8_t can__get_wattemp()
-{
-  return curr_wattemp;
-}
-uint8_t can__get_acctemp() // E car accumulator
-{
-  return curr_acctemp;
-}
-
-// E & C car
-uint8_t can__get_lv()
-{
-  return curr_lv;
-}
 uint8_t can__get_drs()
 {
   return curr_drs;
 }
+
+float can__get_lv()
+{
+  return curr_lv;
+}
+
+#elif (POWERTRAIN_TYPE == 'E')
+float curr_hv = 0;
+float curr_soc = 0;
+float curr_lv = 0;
+float curr_acctemp = 0;
+
+static void can__hv_receive (const CANMessage & inMessage)
+{
+  curr_hv = ((inMessage.data[4]) | (inMessage.data[5] << 8) | (inMessage.data[6] << 16) | (inMessage.data[7] << 24)) * .001f;
+}
+
+static void can__soc_receive (const CANMessage & inMessage)
+{
+//  curr_soc = inMessage.data[1];
+}
+
+static void can__lv_receive (const CANMessage & inMessage)
+{
+//  curr_lv = ((inMessage.data[0]) | (inMessage.data[1] << 8)) * 0.001f; // for e car
+}
+
+static void can__acctemp_receive (const CANMessage & inMessage)
+{
+//  curr_acctemp = inMessage.data[1];
+}
+
+//Accessors
+float can__get_hv()
+{
+  return curr_hv;
+}
+float can__get_soc()
+{
+  return curr_soc;
+}
+
+float can__get_acctemp() // E car accumulator
+{
+  return curr_acctemp;
+}
+
+float can__get_lv()
+{
+  return curr_lv;
+}
+#endif
+
+const ACAN2515Mask rxm0 = standard2515Mask (0x7FF, 0, 0) ;
+//const ACAN2515Mask rxm1 = standard2515Mask (0x7FF, 0, 0) ;
+
+// POWERTRAIN_TYPE == 'C'
+#if (POWERTRAIN_TYPE == 'C')
+const ACAN2515AcceptanceFilter filters [] =
+{
+//  {standard2515Filter (CAN_GEAR_ADDR, 0, 0), can__drs_receive}, // not defined
+//  {standard2515Filter (CAN_GEAR_ADDR, 0, 0), can__oilpress_receive}, // not defined
+  {standard2515Filter (CAN_RPM_ADDR, 0, 0), can__rpm_receive}, // RXF0
+  {standard2515Filter (CAN_GEAR_ADDR, 0, 0), can__gear_receive} // RXF1
+  
+  //{standard2515Filter (0x7FE, 0, 0), can__dummy_receive}, // RXF2
+} ;
+// POWERTRAIN_TYPE == 'E'
+#elif (POWERTRAIN_TYPE == 'E')
+const ACAN2515AcceptanceFilter filters [] =
+{
+  {standard2515Filter (CAN_HV_ADDR, 0, 0), can__hv_receive},
+  {standard2515Filter (CAN_SOC_ADDR, 0, 0), can__soc_receive},
+  {standard2515Filter (CAN_BAT_TEMP_ADDR, 0, 0), can__acctemp_receive},
+  {standard2515Filter (CAN_LV_ADDR, 0, 0), can__lv_receive}
+} ;
+#endif
 
 void can__start()
 {
